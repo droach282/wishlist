c02699bca43dfaffa26a5ac882dd54d5c5f1bb35
Dan Roach <droach282@gmail.com>
2020-01-09 03:51:03 +0000

    Inital commit

---
+++ src/test/java/com/familygeekpod/wishlist/WishlistApplicationTests.java
@@ -0,0 +1,12 @@
+package com.familygeekpod.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistApplicationTests {
+    @Test
+    void contextLoads() {
+
+    }
+}
